f079879836e8d14cc8cd17999bd135c8fb58eada
kagari <kagaricyan@gmail.com>
2024-08-10 12:01:44 +0800

    分页优化，每页尽量不出现展示一半的文字

--- src/pages/book-content/BookContent.vue
+++ src/pages/book-content/BookContent.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="book-content" @click.stop="handlePage">
-    <div class="con" ref="bookContentRef" :style="{fontSize: `${config.fontSize}px`}">
+  <div class="book-content" @click.stop="pageHandler">
+    <div class="read-box" :style="{fontSize: `${config.fontSize}px`}">
       <template v-if="chapterLoading">
         <div class="loading">加载中...</div>
       </template>
-      <template v-else>
-        <p>{{ currentChapterName }}</p>
+      <template v-for="e in currentPageEles">
+        <p>{{ e.innerText}}</p>
       </template>
+    </div>
+    <div class="con" ref="bookContentRef" :style="{fontSize: `${config.fontSize}px`}">
+      <p>{{ currentChapterName }}</p>
       <template v-for="s in bookContentStr.split('\n')">
-        <p>{{ s }}</p>
+        <p style="opacity: 0">{{ s }}</p>
       </template>
     </div>
   </div>
@@ -24,11 +27,12 @@ import {
   updateCurrentReadChapter,
   clearBookChapters,
 } from '../../store';
-import { computed, onBeforeUnmount, onMounted, ref } from 'vue';
+import { computed, nextTick, onBeforeUnmount, onMounted, ref, watch, watchEffect } from 'vue';
 import { getBookContent } from '../../api';
 import MenuDialog from './components/MenuDialog.vue';
-import { useToggle } from '@vueuse/core';
+import { useDebounceFn, useToggle } from '@vueuse/core';
 import router from '../../router/router.ts';
+import { collectPages } from '../../utils';
 
 // 菜单弹窗
 const [menuVisible, toggleMenuVisible] = useToggle()
@@ -37,22 +41,26 @@ const bookContentRef = ref<HTMLDivElement>()
 // 章节内容
 const bookContentStr = ref('');
 // 页面高度
-const pageHeight = window.innerHeight / 100 * 90;
+const pageHeight = Math.floor(window.innerHeight / 100 * 90);
 // 当前滚动位置，下次滚动后，如果位置一致，则切换到上一章/下一章
 let currentScrollTop = 0;
 
 const [chapterLoading, toggleChapterLoading] = useToggle();
+const pageHandler = useDebounceFn((e: MouseEvent) => handlePage(e), 300);
 
 const currentChapterName = computed(() => {
   const index = currentReadBook.value.durChapterIndex;
   return bookChapterList.value.find(i => i.index === index)?.title || ''
 })
+
 /**
  * 翻页
  * 以150px为界限，点击左边向前翻页，点击右边向右翻页，点击中间弹出菜单
  * @param e 点击事件
  */
-const handlePage = async (e: MouseEvent) => {
+const handleScrollPage = async (e: MouseEvent) => {
+  if (chapterLoading.value) return;
+
   if (e.clientX < 150) {
     if (bookContentRef.value) {
       bookContentRef.value.scrollTop = bookContentRef.value.scrollTop - pageHeight;
@@ -75,6 +83,34 @@ const handlePage = async (e: MouseEvent) => {
     toggleMenuVisible(true)
   }
 }
+const currentPage = ref(1)
+const pages = ref<HTMLElement[][]>([]);
+const currentPageEles = computed(() => {
+  return pages.value[currentPage.value-1] || [];
+})
+const handlePage = async (e: MouseEvent) => {
+  if (chapterLoading.value) return;
+
+  if (e.clientX < 150) {
+    if (bookContentRef.value) {
+      if (currentPage.value !== 1) {
+        currentPage.value--
+      } else {
+        await gotoChapter(currentReadBook.value.durChapterIndex - 1)
+      }
+    }
+  } else if (e.clientX > window.innerWidth - 150) {
+    if (bookContentRef.value) {
+      if (currentPage.value !== pages.value.length) {
+        currentPage.value++
+      } else {
+        await gotoChapter(currentReadBook.value.durChapterIndex + 1)
+      }
+    }
+  } else {
+    toggleMenuVisible(true)
+  }
+}
 const gotoChapter = async (index: number) => {
   const bookUrl = currentReadBook.value!.bookUrl;
   if (!bookUrl) {
@@ -86,6 +122,8 @@ const gotoChapter = async (index: number) => {
   }
   bookContentStr.value = ''
   currentScrollTop = 0;
+  currentPage.value = 1
+  pages.value = [];
   updateCurrentReadChapter(index);
   try {
     toggleChapterLoading(true)
@@ -96,6 +134,13 @@ const gotoChapter = async (index: number) => {
     toggleChapterLoading(false)
   }
 }
+
+watch(() => [bookContentStr.value, currentChapterName.value], async () => {
+  await nextTick();
+  if (bookContentRef.value) {
+    pages.value = collectPages(pageHeight, bookContentRef.value.querySelectorAll('.con > p'))
+  }
+})
 onMounted(async () => {
   await queryBookChapters();
   await gotoChapter(currentReadBook.value.durChapterIndex);
@@ -113,7 +158,7 @@ onBeforeUnmount(() => {
   box-sizing: border-box;
   padding: 5vh 20px;
 
-  .con {
+  .con, .read-box {
     overflow: auto;
     width: 100%;
     height: 100%;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,5 @@
 import { ServerUrl } from '../store';
 
-export function createNodeByTemplate(s: string) {
-  const node = document.createElement('div');
-  node.innerHTML = s;
-  return node.firstElementChild!;
-}
-
 export function padNum(s: number) {
   return `0${s}`.slice(-2)
 }
@@ -17,4 +11,48 @@ export function formatDate(s: string | number) {
 
 export function getCover(path: string) {
   return `${ServerUrl}/cover?path=${path}`
+}
+
+export function collectPages(pageHeight: number, elementList: NodeListOf<HTMLElement>) {
+  const sizeNumber = (s: string) => {
+    return Number(s.replace('px', ''))
+  }
+  const computeElementHeight = (element: HTMLElement, beforeElement?: HTMLElement) => {
+    const elementHeight = element.getBoundingClientRect().height;
+    const elementStyle = window.getComputedStyle(element);
+    const elementMarginTop = sizeNumber(elementStyle.marginTop);
+    const elementMarginBottom = sizeNumber(elementStyle.marginBottom);
+    let computeMarginTop = sizeNumber(elementStyle.marginTop);
+    if (beforeElement) {
+      const beforeElementStyle = window.getComputedStyle(beforeElement);
+      const beforeMarginBottom = sizeNumber(beforeElementStyle.marginBottom);
+      if (elementMarginTop <= beforeMarginBottom) {
+        computeMarginTop = 0
+      } else {
+        computeMarginTop = elementMarginTop - beforeMarginBottom
+      }
+    }
+    return {
+      elementHeight: elementHeight + computeMarginTop + elementMarginBottom,
+      elementMarginBottom: elementMarginBottom,
+    }
+  }
+  const pages: HTMLElement[][] = [[]];
+  let currentElementsHeight = 0;
+  for (let i = 0; i < elementList.length; i++) {
+    const element = elementList[i];
+    const currentPageElements = pages[pages.length - 1];
+    const preElement = currentPageElements[currentPageElements.length - 1];
+    const computedElementHeight = computeElementHeight(element, preElement);
+    currentElementsHeight += computedElementHeight.elementHeight
+    if (currentElementsHeight <= pageHeight) {
+      currentPageElements.push(element)
+    } else if (currentElementsHeight - computedElementHeight.elementMarginBottom + 5 <= pageHeight) {
+      currentPageElements.push(element)
+    } else {
+      currentElementsHeight = computedElementHeight.elementHeight;
+      pages.push([element])
+    }
+  }
+  return pages;
 }
